c02f1c4507eb5b8be7d870dbea0b3cd5c0c400d7
Florian Thaler <florian.thaler@edu.uni-graz.at>
2024-11-17 11:11:38 +0100

    finalisation

--- source/data/utils.py
+++ source/data/utils.py
@@ -8,9 +8,11 @@ def visualise_data(data_arr_labeled: np.ndarray, save_figure=False):
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    ax.scatter(data_red[:, 0], data_red[:, 1], c='red', s=1.2, label='class 1')
+    ax.scatter(data_red[:, 0], data_red[:, 1], c='darkred', s=1.2, label='class 1')
     ax.scatter(data_green[:, 0], data_green[:, 1], c='green', s=1.2, label='class 0')
 
-    fig.legend()
+    fig.legend(loc='center right', bbox_to_anchor=(0.95, 0.2))
+    fig.tight_layout(rect=[0, 0, 0.75, 1])
+
     plt.show()
 
